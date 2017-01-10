18c9aea81799f11704250216033958f93a65a2ad
supermy <springclick@gmail.com>
2017-01-10 21:27:06 +0800

    调整编译版本 内嵌调用成功

--- src/main/java/hello/PersonRepository.java
+++ src/main/java/hello/PersonRepository.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.transaction.annotation.Transactional;
 
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
+@Transactional
 public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
 
     List<Person> findByLastName(@Param("name") String name);
